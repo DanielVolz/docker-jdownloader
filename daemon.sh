f43e1bd32ec305279ae3ebfadb5578b8b207564a
Jay MOULIN <jaymoulin@gmail.com>
2019-07-01 12:17:28 +0200

    fixes #32 - Add timeout + waiting for forked PID while updating

--- daemon.sh
+++ daemon.sh
@@ -6,18 +6,30 @@ rm -f /opt/JDownloader/JDownloader.pid
 
 # Debugging helper - if the container crashes, create a file called "jdownloader-block.txt" in the download folder
 # The container will not terminate (and you can run "docker exec -it ... bash")
-if [ -f /root/Downloads/jdownloader-block.txt ]; then
+if [[ -f /root/Downloads/jdownloader-block.txt ]]; then
     sleep 1000000
 fi
 
 # Check if JDownloader.jar exists, or if there is an interrupted update
-if [ ! -f /opt/JDownloader/JDownloader.jar ] && [ -f /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar ]; then
+if [[ ! -f /opt/JDownloader/JDownloader.jar ]] && [[ -f /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar ]]; then
     cp /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar /opt/JDownloader/
 fi
 
 java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &
 PID=$!
-wait $PID
-wait $PID
+echo "PID : ${PID}"
+wait ${PID}
+echo "Process stopped"
+wait ${PID}
+
+# Check if JDownloader is running
+PID=$(pgrep java)
+while [[ "${PID}" ]]; do
+    while [[ "$(ls -la "/proc/${PID}/fd/2" 2> /dev/null | wc -l)" -ge 1 ]]; do
+        echo "Waiting for jdownloader to finish PID : ${PID}"
+        sleep 1
+    done
+    PID=$(pgrep java)
+done
 
 EXIT_STATUS=$?
